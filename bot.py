bda07a3dd2dd6405988b75d5de235cde6835af05
kravosmortis-art <kravosmortis@gmail.com>
2026-08-24 06:45:24 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,10 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Stock Bot is Live!")
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
 def run_web_server():
     port = int(os.environ.get("PORT", 10000))
     server = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
